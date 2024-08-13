a0531ee2146837fe92ee196d1431f6a08b05ea57
uwenji <yw.ji90@gmail.com>
2024-08-13 12:08:03 +0200

    Automated commit message

--- data/cleanCSV.py
+++ data/cleanCSV.py
@@ -3,10 +3,14 @@ import csv
 input_file = '/home/pi/Desktop/BiolabSensors/data/co2.csv'
 output_file = '/home/pi/Desktop/BiolabSensors/data/co2_cleaned.csv'
 
-with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
-    reader = csv.reader(infile)
-    writer = csv.writer(outfile)
+try:
+    with open(input_file, 'r') as infile, open(output_file, 'w', newline='') as outfile:
+        reader = csv.reader(infile)
+        writer = csv.writer(outfile)
 
-    for row in reader:
-        cleaned_row = [field.replace('\n', '').replace('"', '') for field in row]
-        writer.writerow(cleaned_row)
+        for row in reader:
+            cleaned_row = [field.replace('\n', '').replace('"', '') for field in row]
+            writer.writerow(cleaned_row)
+    print("file cleaed and saved successfully.")
+excpet Exception as e:
+    print(f"An error occurred: {e}")
